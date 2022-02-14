f79854be3c50b28bf8ce3b2702d9296b1e67acdb
Taylor Thomas K <taylorthomask@johndeere.com>
2022-02-14 11:08:52 -0600

    add vertical order for game

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ const connectForeGrid.wrapper = [
 // red has the same condition and if all 48 balls are used then it is a tie. 
 const winningLines = [
   //vertical
-[a1, b1, c1, d1, e1, f1, g1, h1]
-
-
-
-
+[h1, g1, f1, e1, d1, c1, b1, a1]
+[h2, g2, f2, e2, d2, c2, b2, a2]
+[h3, g3, f3, e3, d3, c3, b3, a3]
+[h4, g4, f4, e4, d4, c4, b4, a4] 
+[h5, g5, f5, e5, d5, c5, b5, a5]
+[h6, g6, f6, e6, d6, c6, b6, a6]
 ]
 console / log(winningLines);
 
